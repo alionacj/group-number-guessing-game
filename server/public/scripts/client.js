e43f1011426a4a30b8aab671f46418933744e70e
Leo Johnson <leochinonejohnson@gmail.com>
2023-11-02 13:47:29 -0500

    finished state changes.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,29 +1,58 @@
 function onReady() {
   console.log("JavaScript is loaded!")
-}
+} onReady()
+
 
 // when submit is pressed
 function submitGuesses(event) {
 
-  // get input values
+  event.preventDefault();
+
+    // get input values
   let leoGuess = document.getElementById('leo').value;
   let essieGuess = document.getElementById('essie').value;
   let bradenGuess = document.getElementById('braden').value;
   let walkerGuess = document.getElementById('walker').value;
   let markGuess = document.getElementById('mark').value;
   let seanGuess = document.getElementById('sean').value;
-  // make array of guesses
+    // make array of guesses (equal to req.body on server)
   let guessArray = [leoGuess, essieGuess, bradenGuess, walkerGuess, markGuess, seanGuess];
+    console.log('Guess inputs:', guessArray)
 
-  // send post request to server
+    // send post request to server
   axios({
     method: 'POST',
     url: '/guesses',
     data: guessArray
     })
-    // .then((response) => {
-    //     console.log('Confirmed server recieved form submission.')
-    // });
+    .then((response) => {
+          console.log('Confirmed server recieved form submission.')
+          // call info retrieval
+        getGuesses()
+    });
 }
 
-onReady()
+
+// retrieves info from server and updates dom
+function getGuesses() {
+  axios({
+    method: 'GET',
+    url: '/guesses'
+  })
+    // retrieves inventory from response
+  .then(function(response) {
+      console.log('GET response recieved from server! Response:', response);
+    let guessesArray = response.data;
+      console.log('Parsing data from response package. Items:', guessesArray);
+      // formats and edits html with inventory
+    renderDisplay(guessesArray);
+      // for errors
+    }).catch(function(error) {
+        console.log(error);
+        alert('Something bad happened! Check the console for more details.');
+    })
+}
+
+function renderDisplay(guessArray) {
+  console.log('render function!!', guessArray)
+}
--- server/server.js
+++ server/server.js
@@ -16,17 +16,65 @@
 
 
 // RANDOM NUMBER GENERATOR
+// function generateNumber() {
+  let randomNumber = Math.floor(Math.random()*26)
+// }
+
 
 // store guesses
 let guesses = [];
 
+
+// compare inputs to random number
+function compareNumbers(newArray) {
+    // turn inputs into result objects
+    console.log('random number:', randomNumber)
+  let results = [];
+  for (let number of newArray) {
+    if (number > randomNumber){
+      let newguess = {
+        guess: number,
+        result: 'too high'
+      }
+      results.push(newguess);
+    } else if (number < randomNumber) {
+      let newguess = {
+        guess: number,
+        result: 'too low'
+      }
+      results.push(newguess);
+    } else {
+      let newguess = {
+        guess: number,
+        result: 'equal!'
+      }
+      results.push(newguess);
+    }
+  }
+    // updating guesses array with result arrays
+  guesses.push(results)
+    console.log('Results:', results)
+  // send results to client
+}
+
+
 // GET & POST Routes
+
 app.post('/guesses', (req, res) => {
-  console.log('POST request recieved!');
+    console.log('POST request recieved!');
+    // storing inputs into variable
   let newGuesses = req.body;
-  console.log('New item parsed from request.')
-  console.log('New Item:', newGuesses);
-  guesses.push(newGuesses);
-  console.log('Added new guesses to guess list. Updated guesses:', guesses);
+    console.log('New item parsed from request.')
+    console.log('New Item:', newGuesses);
+    console.log('Current guesses:', guesses);
+    // call guess evaluation
+  compareNumbers(newGuesses)
+    // ok :)
   res.sendStatus(201);
 })
+
+app.get('/guesses', (req, res) => {
+    console.log('GET request recieved by client. Sending response...');
+    console.log('Sending guess array to client. Array:', guesses);
+  res.send(guesses);
+});
