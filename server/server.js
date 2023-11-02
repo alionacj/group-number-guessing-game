5db0d2b5a7cce540116b2723d518a1229bcffd18
Leo Johnson <leochinonejohnson@gmail.com>
2023-11-02 11:27:52 -0500

    got server up and running

--- server/server.js
+++ server/server.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const app = express();
-const PORT = 5000;
+// ESTABLISH SERVER
+  const express = require('express');
+  const bodyParser = require('body-parser')
+  const app = express();
+  const PORT = 5000;
+  // Serve up static files (HTML, CSS, Client JS)
+  app.use(express.static('server/public'));
+  // This must be added before GET & POST routes.
+  app.use(bodyParser.urlencoded({extended:true}))
+  // server running
+  app.listen(PORT, () => {
+    console.log ('Server is running on port', PORT)
+  })
+  // read json
+  app.use(express.json());
 
-// This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({extended:true}))
 
-// Serve up static files (HTML, CSS, Client JS)
-app.use(express.static('server/public'));
-
-// GET & POST Routes go here
+// RANDOM NUMBER GENERATOR
 
 
-app.listen(PORT, () => {
-  console.log ('Server is running on port', PORT)
-})
+// GET & POST Routes
